5fbb75d8a43c68da2a615747446d5432b0363b74
Alessandro <alehblack1605@gmail.com>
2024-09-10 18:55:18 -0300

    inicia o mnnu de aplicação

--- index.js
+++ index.js
@@ -1,15 +1,36 @@
-function start() {
-    let count = 1
+const { select } = require('@inquirer/prompts')
+
+const start = async () => {
+    
     while(true){
-        let opção = "sair"
+        
+        const opção = await select({
+            message: "Menu >",
+            choices: [
+                {
+                    name: "Cadastrar meta",
+                    value: "cadastrar"
+                },
+                { 
+                    name: "Listar metas", 
+                    value: "listar"
+                },
+                {
+                    name: "Sair",
+                    value: "sair"
+                }
+            ] 
+        })
+
         switch(opção) {
             case "cadastrar":
-                console.log("Vamos cadastrar")
+                console.log("Meta Cadastrada!")
                 break
             case "listar":
-                console.log("Vamos listar")
+                console.log("Metas: ")
                 break
             case "sair":
+                console.log("Até a próxima!")
                 return
         }  
     }
